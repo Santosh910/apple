1a2cf8bc11ddb149f6ab9821c5d4ea927c2f32b4
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-10-08 13:54:51 +0530

    update

---
+++ myapp/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Routes,Route } from "react-router-dom";
+import Homepage from './component/Homepage/Homepage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/component/Homepage/Homepage.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import './Homepage.css'
+
+const Homepage = () => {
+  return (
+    <div className='container'>
+      <div className="navbar">
+        <div className="nav-list">
+          <ul>
+            <li><i class="fa-brands fa-apple"></i></li>
+            <li>store</li>
+            <li>mac</li>
+            <li>ipad</li>
+            <li>iphone</li>
+            <li>watch</li>
+            <li>airPods</li>
+            <li>TV & Home</li>
+            <li>entertainment</li>
+            <li>accessories</li>
+            <li>support</li>
+            <li><i class="fa-solid fa-magnifying-glass"></i></li>
+            <li><i class="fa-regular fa-bag-shopping"></i></li>
+          </ul>
+        </div>
+
+      </div>
+      <div className="discount">
+        <div className="dis-text">
+          <h5>Get up to ₹8000.00 instant savings on selected products with eligible HDFC Bank cards.*</h5>
+          <a href="#">shop now</a>
+        </div>
+      </div>
+      <div className="backimg-ip">
+        <div className="text-wrp">
+          <h2>iPhone 15 Pro</h2>
+          <h3>Titanium. So strong. So light. So Pro.</h3>
+          <div className="anchor-tag">
+            <a href="#">learn more</a>
+            <a href="#">buy</a>
+          </div>
+        </div>
+
+      </div>
+      <div className="backimg-ip-1">
+        <div className="text-wrp-1">
+          <h2>iPhone 15 </h2>
+          <h3>New camera.New design.Newphoria.</h3>
+          <div className="anchor-tag">
+            <a href="#">learn more</a>
+            <a href="#">buy</a>
+          </div>
+        </div>
+
+      </div>
+      <div className="backimg-ip-2">
+        <div className="text-wrp-2">
+          <img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_medium.png" alt="" />
+          <h3>New camera.New design.Newphoria.</h3>
+          <div className="anchor-tag">
+            <a href="#">learn more</a>
+            <a href="#">buy</a>
+          </div>
+        </div>
+
+      </div>
+      <div className="footer">
+        <div className="footer-1">
+          <div className="footer-1-1">
+            <p>*Instant savings, otherwise referred to as instant cashback on the Apple Store Online, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Terms and conditions apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.</p>
+            <hr />
+          </div>
+          <div className="footer-list">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi fugiat quibusdam eligendi architecto sint adipisci laboriosam dolorem sequi quidem laudantium, ullam animi exercitationem illo modi molestias, quo ea possimus at!</p>
+            <hr />
+          </div>
+          <div className="footer-coprgt">
+            
+            <p>India
+              Copyright © 2023 Apple Inc. All rights reserved.Privacy Policy | Terms of Use | Sales Policy | Legal | Site Map</p>
+              <p>India</p>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
